0387222624589ca7b36ffee8d12d880acf749d5c
dermiie <demioyeniyi@gmail.com>
2025-03-07 22:17:15 +0000

    Installed tailwind and built the structure of the app layout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,5 +1,21 @@
+import { Outlet } from 'react-router';
+import Header from './Header';
+import Footer from './Footer';
+
 function AppLayout() {
-  return <div></div>;
+  return (
+    <div className="layout grid grid-rows-[auto_1fr_auto] h-screen">
+      <Header></Header>
+
+      <div className="overflow-auto">
+        <main className="max-w-3xl mx-auto">
+          <Outlet></Outlet>
+        </main>
+      </div>
+
+      <Footer></Footer>
+    </div>
+  );
 }
 
 export default AppLayout;
