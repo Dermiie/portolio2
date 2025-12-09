d7ef6fc4290977c1190058993e7801a44e0eac1b
dermiie <demioyeniyi@gmail.com>
2025-12-09 19:30:48 +0000

    updated hero text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,17 +4,17 @@ import { ReactTyped } from "react-typed";
 
 
 function Home() {
-  return <div className="flex flex-col md:flex-row gap-3 md:gap-10 justify-center md:items-center h-full px-8 ">
-    <div className="flex md:self-center md:justify-center md:w-1/2 md:rounded-lg md:h-72 md:order-2 justify-center w-32 h-32 mb-8 overflow-hidden rounded-full ring md:ring-2 md:ring-offset-0 ring-offset-8 ring-blue-400">
+  return <div className="flex flex-col justify-center h-full gap-3 px-8 md:flex-row md:gap-10 md:items-center ">
+    <div className="flex justify-center w-32 h-32 mb-8 overflow-hidden rounded-full md:self-center md:justify-center md:w-1/2 md:rounded-lg md:h-80 md:order-2 ring md:ring-2 md:ring-offset-0 ring-offset-8 ring-blue-400">
       <img className="object-cover md:object-center md:self-center" src="/Demi.jpg" alt="Demi"></img>
     </div>
     <div className="md:w-1/2">
       <h1 className="mb-4 text-xl font-extrabold">HI THERE 👋, I&apos;M </h1>
-      <ReactTyped strings={["DEMI OYENIYI"]} typeSpeed={180} backSpeed={40}className="text-2xl font-extrabold text-blue-400 "></ReactTyped>
+      <ReactTyped strings={["DEMILADE OYENIYI"]} typeSpeed={180} backSpeed={40}className="text-2xl font-extrabold text-blue-400 "></ReactTyped>
       <div className="mt-3">
-        <p className="w-[85%] md:w-[70%]text-lg tracking-tight">I&apos;m a frontend developer with a passion for clean code and modern design, I focus on creating seamless user interfaces that are both functional and visually compelling.</p>
+        <p className="w-[85%] md:w-[70%]text-lg tracking-tight">A frontend developer with an eye for clean code and user centric applications, I create seamless user interfaces that are both functional and visually compelling.</p>
         <div className="mt-2 font-bold">
-          <Link to={'/about'} className="hover:text-blue-400 transition-all duration-300">Want to know more about me <span><BiRightArrowAlt         className="text-3xl font-bold text-blue-400"/></span></Link>
+          <Link to={'/about'} className="transition-all duration-300 hover:text-blue-400">Want to know more about me <span><BiRightArrowAlt         className="text-3xl font-bold text-blue-400"/></span></Link>
         </div>   
       </div>
     </div>
--- src/ui/Experience.jsx
+++ src/ui/Experience.jsx
@@ -1,8 +1,21 @@
 function Experience() {
   return (
+   <div className="flex flex-col gap-4 md:gap-6 md:flex-row">
+     <div className="flex flex-col gap-3 p-4 border rounded-sm md:w-2/3 md:mx-auto border-stone-300">
+      <h1 className="tracking-tight text-blue-400 ">Frontend Developer <span className="text-sm text-stone-500 ">Volunteer</span></h1>
+      <h3 className="text-sm tracking-tight text-stone-500">Neoclan Technologies    OCT 2025 - present</h3>
+      <ul className="flex flex-col gap-3 divide-y-2 divide-stone-300">
+      <li>Developed and maintained React + TypeScript components for
+products, cart, and user profiles, ensuring robust and type-safe code.</li>
+      <li>Developed responsive UI features, including conditional rendering and
+micro interaction feedbacks for improved user experience.</li>
+      <li>Collaborated in a Git-based workflow, resolving merge conflicts and
+maintaining type-safe, maintainable code.</li>
+      </ul>
+    </div>
     <div className="flex flex-col gap-3 p-4 border rounded-sm md:w-2/3 md:mx-auto border-stone-300">
       <h1 className="tracking-tight text-blue-400 ">Frontend Developer Intern</h1>
-      <h3 className="text-sm tracking-tight  text-stone-500">RenMoney MFB </h3>
+      <h3 className="text-sm tracking-tight text-stone-500">RenMoney MFB </h3>
       <ul className="flex flex-col gap-3 divide-y-2 divide-stone-300">
       <li>Developed more proficiency in programming languages like
       JavaScript and also the React library</li>
@@ -12,6 +25,7 @@ function Experience() {
       training programs and enforcement of safety procedures.</li>
       </ul>
     </div>
+   </div>
   )
 }
 
