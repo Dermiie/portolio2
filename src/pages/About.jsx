4d5d590d1168de186fd7082af86c0c2632110116
mac <mac@macs-MacBook-Air.local>
2026-02-07 11:20:04 +0100

    improved home page ui

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,19 @@
 import Button from "../ui/Button";
 import Experience from "../ui/Experience";
 import Technologies from "../ui/Technologies";
+import { useLocation } from "react-router-dom";
 
 function About() {
+
+    const location = useLocation();
+  const isAboutPage = location.pathname.includes("about");
+
   return <div className="flex flex-col gap-16 my-4">
-  <div className="flex flex-col items-center  md:gap-3 md:flex-row">
+  <div className="flex flex-col items-center md:gap-3 md:flex-row">
     <div className="flex flex-col md:pt-6 ">
       <header className="text-center text-2xl md:w-[80%] md:mb-4 text-stone-700 font-extrabold md:text-start dark:text-stone-300">About Me</header>
       <h3 className="text-sm tracking-tight md:w-[80%] mb-8 md:mb-4 text-blue-400 md:text-start text-center">Bringing designs to     life with code and creativity! 🎨💻</h3>
-      <div className="self-center w-32 h-32 mb-4 overflow-hidden rounded-full  md:hidden ring ring-offset-8 ring-blue-400">
+      <div className="self-center w-32 h-32 mb-4 overflow-hidden rounded-full md:hidden ring ring-offset-8 ring-blue-400">
       <img src="/Demi.jpg" alt="Demi"></img>
       </div>
       <p className="tracking-tight md:w-[80%] mt-4">Hey there! 👋 <br></br>I&apos;m Demi, a frontend developer passionate about crafting scalable, intuitive, and user-friendly web experiences.<br></br>
@@ -16,9 +21,11 @@ function About() {
       Beyond coding, I&apos;m always exploring new technologies, best practices, and study    materials to improve my skills and stay ahead in the ever-evolving web development space.
       </p>
     </div>
-    <div className="hidden object-contain h-auto max-h-full md:overflow-hidden md:items-start md:pt-6 md:flex">
-      <img className="h-auto " src="/Demi.jpg" alt="Demi"></img> 
-    </div>
+    {isAboutPage && (
+  <div className="self-center w-32 h-32 mb-4 overflow-hidden rounded-full md:hidden ring ring-offset-8 ring-blue-400">
+    <img src="/Demi.jpg" alt="Demi" />
+  </div>
+)}
 </div>
 <div className="flex flex-col">
   <header  className="mb-8 text-lg font-bold text-center md:mb-4">Technologies</header>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router";
 import { BiRightArrowAlt } from "react-icons/bi";
 import { ReactTyped } from "react-typed";
+import About from "./About";
+import Projects from "./Projects";
+import Contact from "./Contact";
 
 
 function Home() {
-  return <div className="flex flex-col justify-center h-full gap-3 px-8 md:flex-row md:gap-10 md:items-center ">
-    <div className="flex justify-center w-32 h-32 mb-8 overflow-hidden rounded-full md:self-center md:justify-center md:w-1/2 md:rounded-lg md:h-80 md:order-2 ring md:ring-2 md:ring-offset-0 ring-offset-8 ring-blue-400">
-      <img className="object-cover md:object-center md:self-center" src="/Demi.jpg" alt="Demi"></img>
+  return <>
+  <div className="flex flex-col justify-center h-full gap-3 px-8 overflow-scroll md:flex-row md:gap-10 md:items-center ">
+    <div className="flex justify-center w-32 h-32 mb-8 overflow-hidden rounded-full md:self-center md:justify-center md:w-1/2 md:h-3/4 md:order-2 ring md:ring-2 md:ring-offset-0 ring-offset-8 ring-blue-400">
+      <img className="object-cover pt-8 md:object-center md:self-center" src="/Demi.jpg" alt="Demi"></img>
     </div>
     <div className="md:w-1/2">
       <h1 className="mb-4 text-xl font-extrabold">HI THERE 👋, I&apos;M </h1>
@@ -20,7 +24,14 @@ function Home() {
     </div>
 
    
-  </div>;
+  </div>
+  <div className="flex flex-col gap-20">
+
+    <About />
+  <Projects />
+  <Contact />
+  </div>
+  </>
 }
 
 export default Home;
